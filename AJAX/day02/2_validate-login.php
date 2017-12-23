8fc95a65af48a0bbf36b23c4e6e2c8b37a6e1b96
dolphin369 <479019769@qq.com>
2017-12-23 18:49:50 +0800

    2 files

--- AJAX/day02/2_validate-login.php
+++ AJAX/day02/2_validate-login.php
@@ -1,5 +1,14 @@
 <?php
  $uname=$_REQUEST["uname"];
  $upwd=$_REQUEST["upwd"];
-    echo "$uname-$upwd";
+    //echo "$uname-$upwd";
+  $conn=mysqli_connect("127.0.0.1","root","","xuezi",3306);
+  $sql="select * from xz_user where uname='$uname' and upwd='$upwd'";
+  $result=mysqli_query($conn,$sql);
+  $row=mysqli_fetch_row($result);
+  if($row==null){
+    echo "用户名或密码不正确";
+  }else{
+    echo "登录成功";
+  }
 ?>
