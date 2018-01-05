42774e33bc3abedd95bd58507b9dda271c5cf4ec
dolphin369 <479019769@qq.com>
2018-01-05 20:09:26 +0800

    files

---
+++ Nodejs/T_resource/day01/5_process.js
@@ -0,0 +1,21 @@
+/**
+ * Created by web-01 on 2017/11/10.
+ */
+
+const os = require('os'); // operating system
+let start = os.uptime();
+// ...
+console.log(os.uptime() - start);
+
+//console.log(process.arch);
+//process.kill(process.pid);
+//console.log(process.platform);
+
+//console.log(process.env);
+//console.log(process.cwd()); // current working directory
+//console.log(process.execPath);
+//console.log(process.versions);
+//console.log(process.uptime());
+//console.log(process.memoryUsage());
+//console.log(process.pid);
+
--- Nodejs/day01/4_console.js
+++ Nodejs/day01/4_console.js
@@ -1,9 +1,10 @@
 /**
  * Created by web-01 on 2018/1/5.
  */
+const Console=require('console').Console;
 const fs=require('fs');
 let out=fs.createWriteStream('out.log');
 let err=fs.createWriteStream('err.log');
 let logger=new console.Console(out,err);
-logger.log('logger log...');
-logger.error('logger error');
+logger.log('logger log123...');
+logger.error('logger error 456');
