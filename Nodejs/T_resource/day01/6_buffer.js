cc278187ff56677dde9735da053ee21e5dd26ea3
dolphin369 <479019769@qq.com>
2018-01-07 12:25:40 +0800

    files

---
+++ Nodejs/T_resource/day01/6_buffer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by web-01 on 2018/1/5.
+ */
+let buf1=new Buffer(1024);
+console.log(buf1);
+let buf2=new Buffer([65,66,67])
+console.log(buf2);
+let buf3=new Buffer('a,b,c,d')
+console.log(buf3);
---
+++ Nodejs/day01/5_process.js
@@ -0,0 +1,15 @@
+/**
+ * Created by web-01 on 2018/1/5.
+ */
+console.log(process.arch);
+console.log(process.platform);
+process.kill(pid);
+
+ console.log(process.cwd());
+ console.log(process.execPath);
+ console.log(process.version);
+ console.log(process.uptime());
+ console.log(process.memoryUsage());
+
+ console.log(process.pid);
+process.kill(pid);
